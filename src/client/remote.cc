5f11010d9413d2fc789df71e053acf40f2b2bc7b
Aki-7 <aki.develop8128@gmail.com>
2022-10-11 16:14:06 +0900

    Introduce gRPC

--- src/client/remote.cc
+++ src/client/remote.cc
@@ -1,10 +1,26 @@
 #include "client/remote.h"
 
+#include <grpcpp/grpcpp.h>
+#include <grpcpp/health_check_service_interface.h>
+
 #include "core/connection/peer.h"
 #include "core/logger.h"
+#include "ping.grpc.pb.h"
+#include "ping.pb.h"
 #include "zen-remote/remote.h"
 
 namespace zen::remote {
+
+// FIXME: This is a temporary code to check if gRPC works.
+class GlobalServiceImpl final : public Global::Service {
+  virtual ::grpc::Status Ping(::grpc::ServerContext* /*context*/,
+      const ::zen::remote::PingReuqest* /*request*/,
+      ::zen::remote::PingResponse* /*response*/) override
+  {
+    LOG_DEBUG("Pong!!");
+    return ::grpc::Status::OK;
+  }
+};
 
 void
 Remote::Start()
@@ -16,6 +32,24 @@ Remote::Start()
         "Server Found: %s", peer_->endpoint().address().to_string().c_str());
   });
   peer_->StartDiscover();
+
+  {  // FIXME: This is a temporary code to check if gRPC works.
+    std::thread([] {
+      std::string server_address("0.0.0.0:50051");
+      GlobalServiceImpl service;
+
+      grpc::EnableDefaultHealthCheckService(true);
+      grpc::ServerBuilder builder;
+
+      builder.AddListeningPort(
+          server_address, grpc::InsecureServerCredentials());
+      builder.RegisterService(&service);
+      std::unique_ptr<grpc::Server> server(builder.BuildAndStart());
+      LOG_DEBUG("gRPC server listening on %s", server_address.c_str());
+
+      server->Wait();
+    }).detach();
+  }
 }
 
 void
